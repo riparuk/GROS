5475598170d74623bb161f6228ac9577ad491f53
riparuk <faruqirifa@gmail.com>
2024-06-07 09:21:25 +0700

    Update needed Data for Request

--- app/src/main/java/com/bukitvista/gros/data/RequestItem.kt
+++ app/src/main/java/com/bukitvista/gros/data/RequestItem.kt
@@ -1,5 +1,15 @@
 package com.bukitvista.gros.data
 
+data class ChecklistItem(
+    val point: String,
+)
+
+data class Checklist(
+    val title: String,
+    val items: List<ChecklistItem>,
+    val completed: Boolean = false
+)
+
 data class RequestItem(
     val timestamp: String,
     val guestName: String,
@@ -8,5 +18,8 @@ data class RequestItem(
     val priority: String,
     val progress: String,
     val staffName: String? = null,
-    val staffImageURL: String? = null
+    val staffImageURL: String? = null,
+    val checklists: List<Checklist>? = null,
+    val imageURLs: List<String>? = null,
+    val notes: String? = null,
 )
